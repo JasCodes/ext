9932fd6d4ee60c023b298aa9dcd06c8e5b3a2315
Jaspreet Singh <mail@jas.bio>
2020-09-26 23:51:13 +0530

    remove context_extensions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
-## [0.0.1] - Genesis.
+## [0.0.2]
+
+* Remove context_extensions due to conflicts with getx extensions
+
+## [0.0.1]
 
 * Added initial extension packages
--- lib/ext.dart
+++ lib/ext.dart
@@ -4,5 +4,5 @@ export 'package:dart_extensions/dart_extensions.dart';
 export 'package:ext/conflict/list_ext.dart';
 export 'package:textstyle_extensions/textstyle_extensions.dart';
 export 'package:collection_ext/all.dart';
-export 'package:context_extensions/context_extensions.dart';
 export 'package:supercharged/supercharged.dart';
+// export 'package:context_extensions/context_extensions.dart'; // TODO: Extract extensions managing conflicts with getx extensions
