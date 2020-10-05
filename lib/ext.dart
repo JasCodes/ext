5d373ea2ca97b90d65b4bb0b777643bc9e858484
Jaspreet Singh <mail@jas.bio>
2020-10-05 17:08:56 +0530

    Add get package extensions

--- lib/ext.dart
+++ lib/ext.dart
@@ -6,3 +6,4 @@ export 'package:textstyle_extensions/textstyle_extensions.dart';
 export 'package:collection_ext/all.dart';
 export 'package:supercharged/supercharged.dart';
 // export 'package:context_extensions/context_extensions.dart'; // TODO: Extract extensions managing conflicts with getx extensions
+export 'package:get/get.dart';
